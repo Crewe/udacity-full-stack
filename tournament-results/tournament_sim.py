082de588101e08bb057cafb58614035a7d7edbe6
Ryan Edwards-Crewe <github@ryanedwardscrewe.com>
2015-05-13 02:13:38 -0400

    Untested Tourney With Byes

--- tournament-results/tournament_sim.py
+++ tournament-results/tournament_sim.py
@@ -58,20 +58,50 @@ def beginTournament(rounds, event_id, has_byes):
     for i in range(rounds):
         matchups = swissPairings(event_id)
         if has_byes:
-            # The last registered person will always have the first bye.
-            first_bye = matchups[len(matchups) - 1][0]
-            if first_bye == match[0] and i == 0:
-                    # First round bye
-                    reportMatch(first_bye, BYE)
+            bye_id = matchups[len(matchups) - 1][0]
+            new_bye_id = 0
+            bye = checkByes(event_id, bye_id)
+
+            # If so give the bye to the next last person who hasn't
+            matches = len(matchups - 1)
+            if bye:
+                j = 0
+                while matches >= 0:
+                    new_bye_id = matchups[matches][0 if j % 2 == 1 else 2]
+                    if !checkByes(event_id, new_bye_id):
+                        # Swap the IDs
+                        matchups[matches][0 if j % 2 == 1 else 2] = bye_id
+                        matchups[len(matchups) - 1][0] = new_bye_id
+                        break
+                    j += 1
+                    if j % 2 == 1:
+                        matches -= 1
+                reportWithBye(event_id, matchups)
+            else:
+                reportWithBye(event_id, matchups)
         else:
             for match in matchups:
                 # Randomly decide who is the winner. 0 = player A wins.
                 winner = int(rand.uniform(0, 2))
-                if winner >= 2:
-                    winner = 1
                 reportMatch(event_id, 
                             match[2 if winner else 0], 
                             match[0 if winner else 2],)
+
+
+def reportWithBye(event_id, matchups):
+    c = len(matchups) - 1
+    b = matchups[c][0]
+    # Report the bye (always the last tuple)
+    report_match(event_id, b, b)
+    # Report the rest
+    for match in matchups:
+        if c < 0
+            break 
+        winner = int(rand.uniform(0, 2))
+        reportMatch(event_id, 
+                    match[2 if winner else 0], 
+                    match[0 if winner else 2],)
+        c -= 1
 
 
 def runSim():
