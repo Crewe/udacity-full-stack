e66ac4b77241ba2b763f29ca043edba81b342dd2
Ryan Edwards-Crewe <github@ryanedwardscrewe.com>
2015-09-24 22:22:17 -0400

    Added user Auth and Client ID
    - Users can now log in with their Google+ account.
    - Lesson 2 Complete

--- scalable-apps-dev/Lesson_2/00_Conference_Central/settings.py
+++ scalable-apps-dev/Lesson_2/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '575360214588-4bd42r7u20ulc736pqq4ohs8hacefp93.apps.googleusercontent.com'
 
