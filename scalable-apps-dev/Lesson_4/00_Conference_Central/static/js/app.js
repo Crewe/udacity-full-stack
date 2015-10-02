46d99cd96e641c4981192b7d0335b89e2da7241d
Ryan Edwards-Crewe <github@ryanedwardscrewe.com>
2015-10-01 23:16:43 -0400

    Todos 1 and 2

--- scalable-apps-dev/Lesson_4/00_Conference_Central/static/js/app.js
+++ scalable-apps-dev/Lesson_4/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '575360214588-4bd42r7u20ulc736pqq4ohs8hacefp93.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
