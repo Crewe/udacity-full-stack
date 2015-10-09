e78420b31345ff4f0a53e26bb24bfac48612cfb3
Ryan Edwards-Crewe <github@ryanedwardscrewe.com>
2015-10-09 04:51:26 -0400

    Added AppStats and Cron job for announcements
    - Added task queue for email to be sent out on new conference creation.
    - Added cron job to display an announcement about conferences that are
    about to fill up.

--- scalable-apps-dev/Lesson_5/00_Conference_Central/conference.py
+++ scalable-apps-dev/Lesson_5/00_Conference_Central/conference.py
@@ -154,7 +154,9 @@ class ConferenceApi(remote.Service):
         # create Conference, send email to organizer confirming
         # creation of Conference & return (modified) ConferenceForm
         Conference(**data).put()
-        # TODO 2: add confirmation email sending task to queue
+        taskqueue.add(params={'email': user.email(),
+                              'conference': repr(request)},
+                      url='/tasks/send_confirmation_email')
 
         return request
 
